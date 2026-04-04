9a1a7c21cfc17ba0af15c3eb4caf15062ed96593
Yuuki Harano <masmer4@yahoo.co.jp>
2026-04-04 22:48:37 +0900

    add noto sans webfont

--- plugins/masm11-theme.rb
+++ plugins/masm11-theme.rb
@@ -19,6 +19,28 @@ add_header_proc do
   EOT
 end
 
+add_header_proc do
+  <<~EOT
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Noto+Sans+Mono:wght@100..900&display=swap" rel="stylesheet">
+    <style>
+      p {
+        font-family: "Noto Sans JP", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 100;
+        font-style: normal;
+      }
+      div.day div.body div.section code {
+        font-family: "Noto Sans Mono", monospace;
+        font-optical-sizing: auto;
+        font-weight: 100;
+        font-style: normal;
+        font-variation-settings: "wdth" 100;
+      }
+    </style>
+  EOT
+end
 # smartphone 用に文字列を短くする。
 # PC でもこれでいい。
 def navi_prev_diary(date); "前の日記"; end
